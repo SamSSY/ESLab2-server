af1d0afdfcfa0be7a0c3ea569040765375e05660
SamSSY <sam7822567@gmail.com>
2015-10-30 23:32:34 +0800

    1030/2332

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(80);
+server.listen(8080, function(){
+    console.log('HTTP on http://127.0.0.1:8080/');
+});
