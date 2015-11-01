a9dee52b49dd4f6802aede5d21d21253f6b4bb4b
SamSSY <sam7822567@gmail.com>
2015-11-01 21:01:07 +0800

    1101/2101

--- server.js
+++ server.js
@@ -2,14 +2,40 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var server = http.createServer(app);
+var port = 8080;
 
 app.use(express.static(__dirname + '/src'));
 
+// GET method route
 app.get('/', function (req, res) {
-  //res.send('Hello World!');
+  //res.send('GET request to the homepage');
+  console.log("get!");
   res.redirect('/view/index.html');
 });
 
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage');
+});
+
+
+var io = require('socket.io')
+			.listen(app.listen(port, function(){
+				console.log('HTTP on http://localhost:8080/');
+			}));
+
+io.sockets.on('connection', function (socket) {
+	console.log("socket!");
+    socket.emit('message', { message: 'welcome! ' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
+
+
+
+/*
 server.listen(8080, function(){
     console.log('HTTP on http://localhost:8080/');
 });
+*/
